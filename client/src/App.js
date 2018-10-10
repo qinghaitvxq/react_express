9c84ed33a49dcff0abfa03fe01b953a59354cff6
yunxi <598616202@qq.com>
2018-10-10 23:47:23 +0800

    lazy-load

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import LazyLoad from 'react-lazyload';
+
 import './App.css';
 
 class App extends Component {
@@ -9,15 +11,19 @@ class App extends Component {
      .then(res=>res.json())
      .then(users=>this.setState({users}))
   }
+ 
   render() {
     return (
       <div className="App">
           <h1>Users</h1>
+          <LazyLoad height={200} offset={100}>
           <ul>
             {this.state.users.map(user =>
               <li key={user.id}>{user.username}</li>
             )}
-          </ul>
+          </ul>      
+          </LazyLoad>
+      
       </div>
     );
   }
